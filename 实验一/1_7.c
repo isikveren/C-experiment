d2cdbb4c5fa99197a05f9f7e36ecd87597de9033
isikveren <lauxunzi@outlook.com>
2021-10-18 20:29:07 +0800

    update

--- 实验一/1_7.c
+++ 实验一/1_7.c
@@ -5,14 +5,13 @@ int main(){
     int num = 1;
     double sum = 0, div, div_up, div_down;
     while(1){
-
         div_up = 2 * num - 1;
         div_down = (2 * num) * (2 * num);
         div = div_up / div_down;
         sum += div;
         if(div <= 0.001){
             break;
-        }             //最后一项小于等于0.001时停止计算
+        }             
         num++;
     }
        printf("n = %d, s = %lf\n",num,sum);
